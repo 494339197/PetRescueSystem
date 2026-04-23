98df456817689ecb72d8fba8866f3f4df37909d1
494339197 <494339197@qq.com>
2026-04-23 15:31:04 +0800

    添加 Spring Boot 后端项目框架

---
+++ src/main/java/com/petrescue/petrescuesystem/PetRescueSystemApplication.java
@@ -0,0 +1,13 @@
+package com.petrescue.petrescuesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetRescueSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetRescueSystemApplication.class, args);
+    }
+
+}
